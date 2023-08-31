c351c4183755101a664a142f631d9ea2bf45628e
ElenaCherpakova <elena.cherpakova@gmail.com>
2023-08-30 21:26:34 -0400

    add useCtities hook

--- worldWise/src/components/CountryList.jsx
+++ worldWise/src/components/CountryList.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 import Spinner from './Spinner';
 import CountryItem from './CountryItem';
 import Message from './Message';
+import { useCities } from '../contexts/CitiesContext';
 
-function CountryList({ cities, isLoading }) {
+function CountryList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
@@ -21,7 +24,7 @@ function CountryList({ cities, isLoading }) {
   return (
     <ul className={styles.countryList}>
       {countries.map((country) => (
-        <CountryItem country={country} key={country.country}/>
+        <CountryItem country={country} key={country.country} />
       ))}
     </ul>
   );
