c2596c7d2d6712f9ba20b730a965d2f47d76f869
ElenaCherpakova <elena.cherpakova@gmail.com>
2023-09-10 20:56:18 -0400

    add map

--- worldWise/src/components/Form.jsx
+++ worldWise/src/components/Form.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './Form.module.css';
 import Button from './Button';
+import BackButton from './BackButton';
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
     .toUpperCase()
@@ -14,10 +15,8 @@ export function convertToEmoji(countryCode) {
 
 function Form() {
   const [cityName, setCityName] = useState('');
-  const [country, setCountry] = useState('');
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState('');
-  const navigate = useNavigate();
 
   return (
     <form className={styles.form}>
@@ -51,14 +50,7 @@ function Form() {
 
       <div className={styles.buttons}>
         <Button type='primary'>Add</Button>
-        <Button
-          type='back'
-          onClick={(e) => {
-            e.preventDefault();
-            navigate(-1);
-          }}>
-          &larr; Back
-        </Button>
+        <BackButton />
       </div>
     </form>
   );
--- worldWise/src/contexts/CitiesContext.jsx
+++ worldWise/src/contexts/CitiesContext.jsx
@@ -6,6 +6,7 @@ const CitiesContext = createContext();
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
 
   useEffect(() => {
     async function fetchCities() {
@@ -22,8 +23,22 @@ function CitiesProvider({ children }) {
     }
     fetchCities();
   }, []);
+
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch (err) {
+      alert('Error fetching cities', err);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
-    <CitiesContext.Provider value={{ cities, isLoading }}>
+    <CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>
       {children}
     </CitiesContext.Provider>
   );
@@ -31,8 +46,8 @@ function CitiesProvider({ children }) {
 
 function useCities() {
   const context = useContext(CitiesContext);
-  if(context === undefined) {
-    throw new Error('useCities must be used within a CitiesProvider')
+  if (context === undefined) {
+    throw new Error('useCities must be used within a CitiesProvider');
   }
   return context;
 }
