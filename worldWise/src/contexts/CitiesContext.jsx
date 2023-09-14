3bd82c1da49a41efbc59725b49ead9a524787aaa
ElenaCherpakova <elena.cherpakova@gmail.com>
2023-09-13 20:13:56 -0400

    add delete function

--- worldWise/src/contexts/CitiesContext.jsx
+++ worldWise/src/contexts/CitiesContext.jsx
@@ -50,15 +50,36 @@ function CitiesProvider({ children }) {
       const data = await res.json();
       setCities((cities) => [...cities, data]);
     } catch (err) {
-      alert('Error fetching cities', err);
+      alert('There is an error creating', err);
     } finally {
       setIsLoading(false);
     }
   }
 
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+      await fetch(`${BASE_URL}/cities/${id}`, {
+        method: 'DELETE',
+      });
+
+      setCities((cities) => cities.filter((city) => city.id !== id));
+    } catch (err) {
+      alert('There was an error deleting', err);
+    } finally {
+      setIsLoading(false);
+    }
+  }
   return (
     <CitiesContext.Provider
-      value={{ cities, isLoading, currentCity, getCity, createCity }}>
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        getCity,
+        createCity,
+        deleteCity,
+      }}>
       {children}
     </CitiesContext.Provider>
   );
