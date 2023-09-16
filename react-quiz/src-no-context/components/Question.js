7da454baecc93fd12e1a591dcb0d41fdd64157a1
ElenaCherpakova <elena.cherpakova@gmail.com>
2023-09-16 15:36:15 -0400

    refactor using context to avoid prop drilling

---
+++ react-quiz/src-no-context/components/Question.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Options from './Options';
+import { useQuiz } from '../context/QuizContext';
+
+function Question() {
+  const { question } = useQuiz()
+  return (
+    <div>
+      <h4>{question.question}</h4>
+      <Options/>
+    </div>
+  );
+}
+
+export default Question;
